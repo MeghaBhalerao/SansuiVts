96b30bc9a819db676968dd88947a917818d936b9
ambiwakchaure <ambiwakchaure.abw@gmail.com>
2019-11-21 12:18:16 +0530

    project all setuo

--- app/src/main/java/vts/snystems/sns/vts/db/DBHelper.java
+++ app/src/main/java/vts/snystems/sns/vts/db/DBHelper.java
@@ -6,17 +6,13 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 
-
-
 /**
  * Created by TEJ on 10/7/2016.
  */
 public class DBHelper extends SQLiteOpenHelper
 {
-        static final String DATABASE_NAME = "esales.db";
-        //static final int DATABASE_VERSION = 1;
-        static final int DATABASE_VERSION = 2;//TABLE_STORE_GEOFENCE created
-        private static final String TAG = "Database";
+        static final String DATABASE_NAME = "sansui_vts.db";
+        static final int DATABASE_VERSION = 1;
 
         public DBHelper(Context context)
         {
@@ -39,14 +35,8 @@ public class DBHelper extends SQLiteOpenHelper
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
         {
-            if(oldVersion == 1 && newVersion == 2)
-            {
-                db.execSQL(TABLE_STORE_GEOFENCE.CREATE_STORE_GEOFENCE);
-                db.execSQL(TABLE_GEOFENCE_VEHICLE_LIST.CREATE_NEW_JOB_TABLE);
 
-            }
         }
-
-    }
+}
 
 
